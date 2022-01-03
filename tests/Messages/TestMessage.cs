3a3ebc441a6a3b923356911e615c456463839eb9
Kezzo <jon_jelinski@t-online.de>
2022-01-03 17:10:12 +0200

    Add variable sized payload to TestMessage

--- tests/Messages/TestMessage.cs
+++ tests/Messages/TestMessage.cs
@@ -6,16 +6,28 @@ namespace ReliableUDP.Tests.Messages;
 public class TestMessage : BaseMessage
 {
     public int ID;
+    public byte[]? Payload;
 
     public override void Deserialize(BinaryReader reader)
     {
         base.Deserialize(reader);
         ID = reader.ReadInt32();
+        var payloadSize = reader.ReadInt32();
+        Payload = reader.ReadBytes(payloadSize);
     }
 
     public override void Serialize(BinaryWriter writer)
     {
         base.Serialize(writer);
         writer.Write(ID);
+
+        if(Payload == null)
+        {
+            writer.Write(0);
+            return;
+        }
+        
+        writer.Write(Payload.Length);
+        writer.Write(Payload);
     }
 }
